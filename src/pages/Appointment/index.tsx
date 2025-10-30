6f4ef95ef3d89230b87fb629ce58bd369ec5808d
guojiuling <514360255@qq.com>
2025-10-30 17:05:50 +0800

    feat: 1、预约列表所属人查询条件

--- src/pages/Appointment/index.tsx
+++ src/pages/Appointment/index.tsx
@@ -4,16 +4,32 @@
  * @Description:
  */
 
+import { queryAdminUserList } from '@/api/account';
 import { queryAppointmentPage } from '@/api/appointment';
 import { CustomColumnProps } from '@/components/compontent';
 import CustomTable from '@/components/CustomTable';
 import { USER_INFO_KEY } from '@/constants';
+import { transformValueEnum } from '@/utils';
 import Local from '@/utils/store';
+import { useEffect, useState } from 'react';
 
 const Appointment = () => {
   const userInfo = Local.get(USER_INFO_KEY);
   const isAdmin = userInfo.roleCode === 'SUPER_ADMIN';
-  const columns: CustomColumnProps[] = [
+  const [columns, setColumns] = useState<CustomColumnProps[]>([
+    {
+      title: '所属人',
+      dataIndex: 'accountId',
+      hideInSearch: !isAdmin,
+      valueType: 'select',
+      hideInTable: true,
+    },
+    {
+      title: '所属人',
+      dataIndex: 'accountName',
+      hideInSearch: true,
+      hideInTable: !isAdmin,
+    },
     {
       title: '医院名称',
       dataIndex: 'hospitalName',
@@ -56,13 +72,15 @@ const Appointment = () => {
       hideInSearch: true,
       ellipsis: true,
     },
-    {
-      title: '所属人',
-      dataIndex: 'accountName',
-      hideInSearch: !isAdmin,
-      hideInTable: !isAdmin,
-    },
-  ];
+  ]);
+
+  useEffect(() => {
+    if (isAdmin) {
+      queryAdminUserList().then((data: any) => {
+        setColumns(transformValueEnum(columns, data, 'accountId'));
+      });
+    }
+  }, []);
 
   return <CustomTable columns={columns} request={queryAppointmentPage} />;
 };
